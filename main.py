b7c38fed5e5f5f6d7c023640ddd4848f9ddca395
Aldas <40773946+AXDZ@users.noreply.github.com>
2020-04-01 00:45:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,9 @@ import requests
 import asyncio
 from termcolor import colored
 from colorama import init
-from proxyscrape import create_collector
-from fake_useragent import UserAgent
+import re
 
-#gets useragent that we could use
-ua = UserAgent()
-
-#makes proxies collector
-collector = create_collector('my-collector', 'https')
+sviews = 0
 
 #Makes this work for windows
 init()
@@ -37,7 +32,7 @@ def clear():
 #function to view profiles
 async def view(username, number=None, proxies=None, headers=None, timeout=None):
     if number == None:
-        r = requests.get(f"https://namemc.com/profile/{username}", headers=headers, proxies=proxies, timeout=timeout)
+        r = requests.get(f"https://namemc.com/profile/{username}", headers=headers, proxies={"http": proxy,"https": proxyif }, timeout=timeout)
         return r.status_code
     else:
         r = requests.get(f"https://namemc.com/profile/{username}.{number}", headers=headers, proxies=proxies, timeout=timeout)
@@ -65,7 +60,7 @@ while name_input:
         #if user enters anything else
         else:
             pass
-        
+
         #prints text to let users know that its not really needed
         print(colored("Press enter for 1 (default) as user number", "magenta"))
         #user number input
@@ -73,34 +68,63 @@ while name_input:
         #if user presses enter (skips)
         if usernumber == "":
             usernameid = None
+            name_input = False
         #if user enters anything else
         else:
-            pass
-        
-        #defines proxy
-        proxy = collector.get_proxy()
-        port = proxy[1]
-        proxy = proxy[0]
-        proxy = f"{proxy}:{port}"
-        #sends a reuqest
-        r_status_code = asyncio.run(view(username=username, number=usernumber, proxies={"https":proxy}, headers={'user-agent': ua.random}, timeout=10))
-        #if we get 200 as a status code (success)
-        if r_status_code == 200:
-            sviews += 1
-            print(colored(f"[{sviews} sent]", "magenta") + " " + colored('Successfully sent a view', 'green'))
-        #if 404 error occurs (page not found error)
-        elif r_status_code == 404:
-            print(colored("No such a profile", "red"))
-            exit()
-        #if request status code is something else
-        else:
-            print(colored(f"Sent a request and got status code of {r_status_code}", "magenta"))
-                
-    #if you press ctrl + c
+            name_input = False
+
+    # if you press ctrl + c
     except KeyboardInterrupt:
         print(colored("Bye!", "green"))
         exit()
-    
-    #if any other error occurs
+
+    # if any other error occurs
+    except Exception as e:
+        print(f"{e}\n{colored('Error has occured! ^')}")
+
+runnin = True
+while runnin:
+    try:
+        #defines proxy
+        with open("proxies.txt") as f:
+            for i in f.readlines():
+                proxy = i.rstrip()
+                proxies = {
+                    "http": proxy,
+                }
+                print(proxy)
+                headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                           "Accept-Encoding": "gzip, deflate, br",
+                           "Accept-Language": "en-US,en;q=0.5",
+                           "Cache-Control": "max-age=0",
+                           "Connection": "keep-alive",
+                           "DNT": 1,
+                           "Host": "namemc.com",
+                           "Referer": f"https://namemc.com/search?q={username}",
+                           "TE": "Trailers",
+                           "Upgrade-Insecure-Requests": 1,
+                           "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0",
+                           }
+                #sends a reuqest
+                req = asyncio.run(view(username=username, number=usernumber, proxies=proxy, headers={'User-Agent': ua.random}, timeout=10))
+                #if we get 200 as a status code (success)
+                if req == 200:
+                    sviews += 1
+                    print(colored(f"[{sviews} views sent]", "magenta") + " " + colored('Successfully sent a view', 'green'))
+                #if 404 error occurs (page not found error)
+                elif req == 404:
+                    print(colored("No such a profile", "red"))
+                    exit()
+                #if request status code is something else
+                else:
+                    print(colored(f"Sent a request and got status code of {req}", "magenta"))
+                    continue
+
+    # if you press ctrl + c
+    except KeyboardInterrupt:
+        print(colored("Bye!", "green"))
+        exit()
+
+    # if any other error occurs
     except Exception as e:
         print(f"{e}\n{colored('Error has occured! ^')}")
